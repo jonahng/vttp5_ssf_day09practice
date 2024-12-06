17f4bb9f92d1525ff2f6d016c595902165616f4a
jonahng <nyhj1998@gmail.com>
2024-12-06 16:08:13 +0800

    created form and receive the form input

--- src/main/java/com/jonah/vttp5_ssf_day09practice/Controller/ToDoController.java
+++ src/main/java/com/jonah/vttp5_ssf_day09practice/Controller/ToDoController.java
@@ -3,12 +3,21 @@ package com.jonah.vttp5_ssf_day09practice.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.jonah.vttp5_ssf_day09practice.Constants.Constants;
+import com.jonah.vttp5_ssf_day09practice.Model.ToDo;
 import com.jonah.vttp5_ssf_day09practice.Service.ToDoRestService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -22,6 +31,32 @@ public class ToDoController {
         model.addAttribute("alltodo", toDoRestService.allToDoFromRedis(Constants.todoKey));
         return "alltodo";
     }
+
+
+    @GetMapping("/createtodo")
+    public String createToDo(Model model){
+        ToDo todo = new ToDo();
+        model.addAttribute("todo", todo);
+        return "createtodoform";
+    }
+
+
+    @PostMapping("/createtodo")
+    public String postCreateToDo(@Valid @ModelAttribute("todo") ToDo todo, BindingResult bindingResult, Model model) {
+        //TODO: process POST request
+        if(bindingResult.hasErrors()){
+            System.out.println("for binding result has errors");
+            return "createtodoform";
+        }
+        System.out.println("post form received from createTODO.:" + todo);
+
+        //json builder to turn the received person into json object
+        //write json object into the redis database susing maprepo?
+
+        return "INSERT STRING";
+
+    }
+    
     
 
 
