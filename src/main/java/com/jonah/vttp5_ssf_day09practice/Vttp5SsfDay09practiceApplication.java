d0a1432da5868f1d96662ebc8b2cfe1b458dd653
jonahng <nyhj1998@gmail.com>
2024-12-06 13:41:10 +0800

    spring template

---
+++ src/main/java/com/jonah/vttp5_ssf_day09practice/Vttp5SsfDay09practiceApplication.java
@@ -0,0 +1,13 @@
+package com.jonah.vttp5_ssf_day09practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5SsfDay09practiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5SsfDay09practiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonah/vttp5_ssf_day09practice/Vttp5SsfDay09practiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonah.vttp5_ssf_day09practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5SsfDay09practiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
